96d873769708c998de60d0d28a6fd6f642bbca1b
CSarroca <sarroca93@gmail.com>
2022-09-05 14:57:44 +0200

    Added test for cardsRouter

--- src/server/routers/cardsRouter.ts
+++ src/server/routers/cardsRouter.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import getAllCards from "../controllers/cards/cardsControllers";
+import authentication from "../middlewares/authentication";
 
 const cardsRouter = express.Router();
 
-cardsRouter.get("/", getAllCards);
+cardsRouter.get("/", authentication, getAllCards);
 
 export default cardsRouter;
