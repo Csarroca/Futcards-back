76da48ee86b12badb739d96de91c6292c6e4bae6
CSarroca <sarroca93@gmail.com>
2022-09-06 22:37:02 +0200

    Added path to cardsRouter for deleteById

--- src/server/routers/cardsRouter.ts
+++ src/server/routers/cardsRouter.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { getAllCards } from "../controllers/cards/cardsControllers";
+import { deleteById, getAllCards } from "../controllers/cards/cardsControllers";
 import authentication from "../middlewares/authentication";
 
 const cardsRouter = express.Router();
 
 cardsRouter.get("/", authentication, getAllCards);
+cardsRouter.delete("/:id", deleteById);
 
 export default cardsRouter;
