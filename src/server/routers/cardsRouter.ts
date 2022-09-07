a6534cd31f5cfe40a757014bf052022bf11adc3f
CSarroca <sarroca93@gmail.com>
2022-09-07 21:04:32 +0200

    Created endpoint route in cardRouter

--- src/server/routers/cardsRouter.ts
+++ src/server/routers/cardsRouter.ts
@@ -1,10 +1,15 @@
 import express from "express";
-import { deleteById, getAllCards } from "../controllers/cards/cardsControllers";
+import {
+  createCard,
+  deleteById,
+  getAllCards,
+} from "../controllers/cards/cardsControllers";
 import authentication from "../middlewares/authentication";
 
 const cardsRouter = express.Router();
 
 cardsRouter.get("/", authentication, getAllCards);
 cardsRouter.delete("/:id", deleteById);
+cardsRouter.post("/create", createCard);
 
 export default cardsRouter;
