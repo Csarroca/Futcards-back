44b2d9a54fcef47b64a2bd2093ebefa638e04d15
CSarroca <sarroca93@gmail.com>
2022-09-07 23:25:01 +0200

    Added test for parseData function

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -37,3 +37,22 @@ export interface CardData {
   foot: string;
   owner: string;
 }
+
+export interface ProtoCardData {
+  name: string;
+  image: string;
+  position: string;
+  nacionallity: string;
+  team: string;
+  overall: number;
+  physicall: number;
+  pace: number;
+  passing: number;
+  defense: number;
+  shooting: number;
+  dribbling: number;
+  height: number;
+  age: number;
+  foot: string;
+  owner: string;
+}
