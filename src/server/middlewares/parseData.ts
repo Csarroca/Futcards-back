ec123d3a1cf08fb73be49badc69ee3d752c1c7ff
CSarroca <sarroca93@gmail.com>
2022-09-12 10:24:49 +0200

    Fixed problem with image at create card

--- src/server/middlewares/parseData.ts
+++ src/server/middlewares/parseData.ts
@@ -7,18 +7,17 @@ import createCustomError from "../../utils/createCustomError/createCustomError";
 const parseData = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const newCard = req.body.card;
-
     const cardObject = await JSON.parse(newCard);
 
     const newName = `${Date.now()}${req.file.originalname}`;
-    cardObject.picture = newName;
+    cardObject.image = newName;
 
     await fs.rename(
       path.join("uploads", req.file.filename),
       path.join("uploads", newName)
     );
 
-    cardObject.image = req.file.filename;
+    cardObject.image = newName;
 
     req.body = cardObject;
 
